c464a3ea8ccea6d30115c4dab321a3100a966343
Andreas <andreas_vso@hotmail.com>
2013-07-24 09:52:32 +0200

    blerg

--- grid/generate_finer_mesh.py
+++ grid/generate_finer_mesh.py
@@ -1,6 +1,6 @@
 from dolfin import *
 
-mesh = Mesh('meshes/reference.xml')
+mesh = Mesh('meshes/reference.xml.gz')
 mesh = refine(mesh)
 
 print mesh.num_cells()
@@ -9,6 +9,10 @@ print mesh.num_vertices()
 file = File('meshes/reference_finer.xml')
 file << mesh
 
+mesh = refine(mesh)
+
+file = File('meshes/reference_finer_finer.xml')
+file << mesh
 
 mesh = Mesh('meshes/reference_finer_finer.xml')
 print mesh.num_cells()
